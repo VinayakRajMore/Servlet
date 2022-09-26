fd79dd97811bc35e7b556acda8109f243abe88e9
VinayakRaj More <112251235+VinayakRajMore@users.noreply.github.com>
2022-09-26 20:25:53 +0530

    Add files via upload

---
+++ log_In/src/main/java/LogIn.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogIn extends HttpServlet {
+
+	public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter writer = resp.getWriter();
+		writer.println("Welcome to techno elevate");
+		
+		
+		String string = req.getParameter("name");
+		String string2 = req.getParameter("password");
+		
+		writer.println(string);
+		writer.println(string2);
+		
+	}
+
+}
